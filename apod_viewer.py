de0d68cb2d4b034eff079f4eb6cb90a032746f6a
Joelle Waugh <joelle.waugh@flemingcollege.ca>
2024-08-11 20:34:30 -0400

    update

--- apod_viewer.py
+++ apod_viewer.py
@@ -11,5 +11,15 @@ root.minsize(500,500)
 root.columnconfigure(0,weight=1)
 root.rowconfigure(0, weight=1)
 
+# TODO: Set the icon
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('COMP593.NasaIMageViewer')#
+root.iconbitmap(os.path.join(script_dir, 'Nasa.ico'))#
+
+# TODO: Create frames
+frm = ttk.Frame(root)
+frm.columnconfigure(0, weight=1)
+frm.rowconfigure(0, weight=1)
+frm.grid(sticky= NSEW)
+
 
 root.mainloop()
